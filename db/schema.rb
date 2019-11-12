d04d127114f03b74ad47ba28c0b0e0c15466d143
Stefanelli <stefanellisimon@gmail.com>
2019-11-12 16:01:28 +0100

    seed final, user complet, user assigné aléatoirement à un bateau

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_120604) do
+ActiveRecord::Schema.define(version: 2019_11_12_143548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,13 +22,13 @@ ActiveRecord::Schema.define(version: 2019_11_12_120604) do
     t.string "city"
     t.float "long"
     t.float "lat"
-    t.text "description"
-    t.integer "capacity"
     t.integer "price"
     t.boolean "available"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "capacity"
+    t.text "description"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 2019_11_12_120604) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.string "photo"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# réiinitialisation de la db
+puts "On va écraserla DB les frères"
+Boat.destroy_all
+User.destroy_all
+ @toto = User.create!(
+    email: 'admin@gmail.com',
+    password: 'password',
+    username: 'admin',
+    last_name: 'admin',
+    first_name: 'admin',
+    photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjF08e39-TlAhUCyoUKHcMLCYMQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.business2community.com%2Fsocial-media%2Fimportance-profile-picture-career-01899604&psig=AOvVaw0KDssQW0s4CdXmmxKLFWzp&ust=1573657036943224',
+    description: "describe your self lazy admin..."
+  )
+
+# SEED USER :
+puts "Creation de personnage super cool !"
+
+portrait = ["https://images.unsplash.com/photo-1546538994-4f15d0aa966f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60",
+            "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+            "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+            "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+            "https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"]
+
+i = 0
+5.times do
+  username = Faker::Games::Pokemon.name
+  User.create!(
+    username: username,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password",
+    description: "hi my name is #{username} one day i met yoda and he says to me '#{Faker::Quote.yoda}' but Chuck explain him that #{Faker::ChuckNorris.fact}",
+    photo: portrait[i])
+    i += 1
+end
+puts "Chuck Norris Win !"
+
 
 #SEED BOAT :
 # require 'uri'
@@ -12,10 +43,6 @@
 index_url1 = "https://www.clickandboat.com/location-bateau/france/marseille"
 index_url2 = "https://www.clickandboat.com/location-bateau/france/brest"
 
-puts "On va écraserla DB les frères"
-Boat.destroy_all
-User.destroy_all
- @toto = User.create!(email: 'admin@gmail.com', password: 'adminou')
 
 def fetch_boat(url)
   html_file = open(URI::encode(url)).read
@@ -45,10 +72,14 @@ def fetch_boat(url)
    end
 
 
+
+
    booty = Boat.new(@args_boats)
-   booty.user = @toto
+   random_user = User.all.sample
+   booty.user = random_user
    booty.save!
 
+puts "le bateau : #{booty.name} appartient : #{random_user.username}"
 
 end
 
@@ -73,30 +104,7 @@ end
 fetch_index(index_url1)
 fetch_index(index_url2)
 
+
 puts "La puissance du port du Havre"
 
 
-# SEED USER :
-
-puts "Creation de personnage super cool !"
-
-portrait = ["https://images.unsplash.com/photo-1546538994-4f15d0aa966f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60",
-            "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-            "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-            "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-            "https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"]
-
-i = 0
-5.times do
-  username = Faker::Games::Zelda.character
-  User.create!(
-    username: username,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: "password",
-    description: "hi my name is #{username} one day i met yoda and he says to me '#{Faker::Quote.yoda}' but Chuck explain him that #{Faker::ChuckNorris.fact}",
-    photo: portrait[i])
-    i += 1
-end
-puts "Chuck Norris Win !"
