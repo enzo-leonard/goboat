92d97ad91dfbfba015c951d222dda8f0116745d5
Arnaud <arnaud.benveniste@gmail.com>
2019-11-12 12:45:18 +0100

    Scrap seeds and column added to Boat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_151840) do
+ActiveRecord::Schema.define(version: 2019_11_12_103325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,19 @@ ActiveRecord::Schema.define(version: 2019_11_11_151840) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "capacity"
+    t.text "description"
     t.index ["user_id"], name: "index_boats_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "boat_id", null: false
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["boat_id"], name: "index_bookings_on_boat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +55,6 @@ ActiveRecord::Schema.define(version: 2019_11_11_151840) do
   end
 
   add_foreign_key "boats", "users"
+  add_foreign_key "bookings", "boats"
+  add_foreign_key "bookings", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#SEED BOAT :
+require 'uri'
+
+index_url1 = "https://www.clickandboat.com/location-bateau/france/marseille"
+index_url2 = "https://www.clickandboat.com/location-bateau/france/brest"
+
+puts "On va écraserla DB les frères"
+Boat.destroy_all
+User.destroy_all
+ @toto = User.create!(email: 'admin@gmail.com', password: 'adminou')
+
+def fetch_boat(url)
+  html_file = open(URI::encode(url)).read
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('h2').each do |element|
+    @args_boats[:name] = element.text.strip
+  end
+  html_doc.search('#general > div.key-info > span.icon-user-single-a-group.col-3-12.mobile-col-6-12').each do |element|
+    @args_boats[:capacity] = element.text.strip
+  end
+  html_doc.search('p').each do |element|
+   @args_boats[:description] = element.text.strip
+    break
+  end
+   html_doc.search('.picSlide').each_with_index do |element, index|
+   if index == 0
+    a = element.attribute('style').value.gsub('background-image: url(//', '').gsub(');', '')
+    @args_boats[:photo] = a
+  end
+  end
+
+  html_doc.search('#map').each do |elem|
+    @args_boats[:lat] = elem.attribute('data-img-src').value.match(/([=]\d{1,}.\d{1,})/).to_s.gsub('=', '')
+  end
+   html_doc.search('#map').each do |elem|
+   @args_boats[:long] = elem.attribute('data-img-src').value.match(/([C]\d{1,}\W\d{1,})/).to_s.gsub('C', '')
+   end
+
+
+   booty = Boat.new(@args_boats)
+   booty.user = @toto
+   booty.save!
+
+
+end
+
+def fetch_index(url)
+  @args_boats = {}
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+
+  html_doc.search('span.price').each do |elem|
+    @args_boats[:price] = elem.text.strip
+  end
+  html_doc.search('span.textReg').each do |elem|
+    @args_boats[:city] = elem.text.strip
+  end
+  html_doc.search('.product-link').each_with_index do |element, i|
+    fetch_boat(element.attribute('href').value)
+  end
+end
+
+
+
+fetch_index(index_url1)
+fetch_index(index_url2)
+
+puts "La puissance du port du Havre"
+
+
+#SEED USER A CREER A LA SUITE
+
+
