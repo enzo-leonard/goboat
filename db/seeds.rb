656eafb1b1dea846d58375e0d55bad6719145c5f
Stefanelli <stefanellisimon@gmail.com>
2019-11-12 15:42:39 +0100

    correction de la création de user seed. Ajout d'une photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #SEED BOAT :
-require 'uri'
+# require 'uri'
 
 index_url1 = "https://www.clickandboat.com/location-bateau/france/marseille"
 index_url2 = "https://www.clickandboat.com/location-bateau/france/brest"
@@ -39,7 +39,6 @@ def fetch_boat(url)
 
   html_doc.search('#map').each do |elem|
     @args_boats[:lat] = elem.attribute('data-img-src').value.match(/([=]\d{1,}.\d{1,})/).to_s.gsub('=', '')
-    p @args_boats[:lat]
   end
    html_doc.search('#map').each do |elem|
    @args_boats[:long] = elem.attribute('data-img-src').value.match(/([C]\d{1,}\W\d{1,})/).to_s.gsub('C', '')
@@ -77,14 +76,27 @@ fetch_index(index_url2)
 puts "La puissance du port du Havre"
 
 
-#SEED USER :
+# SEED USER :
 
+puts "Creation de personnage super cool !"
+
+portrait = ["https://images.unsplash.com/photo-1546538994-4f15d0aa966f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60",
+            "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+            "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+            "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+            "https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"]
+
+i = 0
 5.times do
-  User.create(
-    username: Faker::Games::Zelda.character,
+  username = Faker::Games::Zelda.character
+  User.create!(
+    username: username,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
-    description: "hi my name is #{first_name} one day i met yoda and he says to me '#{Faker::Quote.yoda}' but Chuck explain him that #{Faker::ChuckNorris.fact}"
-
+    password: "password",
+    description: "hi my name is #{username} one day i met yoda and he says to me '#{Faker::Quote.yoda}' but Chuck explain him that #{Faker::ChuckNorris.fact}",
+    photo: portrait[i])
+    i += 1
 end
+puts "Chuck Norris Win !"
