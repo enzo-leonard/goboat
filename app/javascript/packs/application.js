4202ac52c382a8bddc5517579ff64d3527b73b79
Melvin <melvin.vmegen@gmail.com>
2019-11-14 10:57:56 +0100

    whatever

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,18 @@ import "bootstrap";
 // Begin sticky bar
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-// window.onscroll = function () { scrollFunction() };
+window.onscroll = function () { scrollFunction() };
 
-// const navbar = document.getElementById("sticky-header");
-// const sticky = navbar.offsetTop;
+const navbar = document.getElementById("sticky-header");
+const sticky = navbar.offsetTop;
 
-// function scrollFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky-header")
-//   } else {
-//     navbar.classList.remove("sticky-header");
-//   }
-// };
+function scrollFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky-header")
+  } else {
+    navbar.classList.remove("sticky-header");
+  }
+};
 
 
 import initMapbox from '../plugins/init_mapbox'
