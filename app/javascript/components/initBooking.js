8880fd96e8c17d4559f6805cb35e1d93b8e1b01b
Melvin <melvin.vmegen@gmail.com>
2019-12-02 11:36:11 +0100

    fixe pb login

--- app/javascript/components/initBooking.js
+++ app/javascript/components/initBooking.js
@@ -1,4 +1,3 @@
-
 const initBooking = () => {
   const date_range = document.querySelector('#daterange')
 
@@ -11,63 +10,45 @@ const initBooking = () => {
       change.click
 
 
-    change.addEventListener('click', () => {
-      const range_value = document.querySelector('.drp-selected').innerHTML
-      const date_begin = range_value.split('-')[0]
-      const date_end = range_value.split('-')[1]
-      const nb_day = document.querySelector('#nb-day');
-      const sended_price = document.querySelector('#sended-price')
-      const prix = document.querySelector('#prix');
-      const price = document.querySelector('#price').innerHTML;
-      const date1 = new Date(date_begin)
-      const date2 = new Date(date_end)
+      change.addEventListener('click', () => {
+        const range_value = document.querySelector('.drp-selected').innerHTML
+        const date_begin = range_value.split('-')[0]
+        const date_end = range_value.split('-')[1]
+        const nb_day = document.querySelector('#nb-day');
+        const sended_price = document.querySelector('#sended-price')
+        const prix = document.querySelector('#prix');
+        const price = document.querySelector('#price').innerHTML;
+        const date1 = new Date(date_begin)
+        const date2 = new Date(date_end)
 
-      console.log(date_begin)
-      console.log(date_end)
+        console.log(date_begin)
+        console.log(date_end)
 
-      const diffTime = Math.abs(date2 - date1);
-      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-      console.log(diffDays);
+        const diffTime = Math.abs(date2 - date1);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        console.log(diffDays);
 
-      nb_day.innerHTML = diffDays
-      prix.innerHTML = price * diffDays
-      sended_price.value = price * diffDays
-    })
-
-
-  }
-
-
-
-
-
+        nb_day.innerHTML = diffDays
+        prix.innerHTML = price * diffDays
+        sended_price.value = price * diffDays
+      })
+    }
   });
+  // const commit = document.querySelector('#new_booking');
+  // commit.addEventListener('click', (event) => {
+  //   console.log(event)
+  //   event.preventDefault()
+  // })
 
+  $('form').submit(function () {
+    const container = document.querySelector('.booking-container')
+    const title = document.querySelector('#title-booking')
+    console.log(title)
+    title.innerHTML = "Bateau réservé !"
+    title.style.color = 'green'
 
-  const commit = document.querySelector('form');
-
-
-
-
-   commit.addEventListener('click', (event) => {
-     event.preventDefault()
-     event
-   })
-
-   $('form').submit(function () {
-     const container = document.querySelector('.booking-container')
-     const title = document.querySelector('#title-booking')
-     console.log(title)
-     title.innerHTML = "Boat hooked !"
-     title.style.color = 'green'
-     container.style.border = "2px solid green"
-     container.style.height = "70px";
-
-     return true;
-   });
-
-
-
+    return true;
+  });
 }
 
 export default initBooking
